8cd69ea0de1c97070ae508b1ea11b6cb8857bc31
Surma <surma@surma.link>
2018-08-06 14:41:52 +0100

    Properly detect entry modules

--- index.js
+++ index.js
@@ -14,13 +14,23 @@
 const { readFileSync } = require("fs");
 const { dirname, join } = require("path");
 
+function isEntryModule(chunk, inputs) {
+  return chunk.orderedModules.some(module => inputs.includes(module.id));
+}
+
 module.exports = function(opts = {}) {
   if (!opts.loader) {
     const loaderPath = join(__dirname, "/loader.js");
     opts.loader = readFileSync(loaderPath);
   }
+  let inputs;
+  let resolvedInputs;
   return {
     name: "loadz0r",
+
+    options({input}) {
+      inputs = input;
+    },
 
     transformChunk(code, outputOptions, chunk) {
       if (outputOptions.format !== "amd") {
@@ -40,13 +50,19 @@ module.exports = function(opts = {}) {
       }
       // And add the `define(` back in with the module name inlined.
       code = `define("${id}", ${code}`;
-      // If this is an entry module, add the loader code.
-      // FIXME(@surma): This also adds the loader to named chunks and I have
-      // no idea how to fix it.
-      if (chunk.isEntryModuleFacade) {
-        code = opts.loader + code;
+
+      // If not already done, resolve input names to fully qualified moduled IDs
+      if(!resolvedInputs) {
+        resolvedInputs = Promise.all(inputs.map(id => this.resolveId(id)));
       }
-      return { code, map: null };
+      return resolvedInputs
+        .then(inputs => {
+          // If this is an entry module, add the loader code.
+          if(isEntryModule(chunk, inputs)) {
+            code = opts.loader + code;
+          }
+          return { code, map: null };
+        });
     }
   };
 };
