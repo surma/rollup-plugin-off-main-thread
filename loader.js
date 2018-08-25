71217668de046c647d43a945973c2902b44be2c3
Rich Harris <richard.a.harris@gmail.com>
2018-08-25 17:09:47 -0400

    implement publicPath option

--- loader.js
+++ loader.js
@@ -29,7 +29,11 @@
       await new Promise(async resolve => {
         if ("document" in self) {
           const script = document.createElement("script");
+          // #ifdef publicPath
+          script.src = // #put "'" + publicPath + "' + name.slice(1);"
+          // #else
           script.src = name;
+          // #endif
           // Ya never know
           script.defer = true;
           document.head.appendChild(script);
